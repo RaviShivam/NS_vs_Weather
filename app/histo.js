978b4771f1316d882c74f15937974d0b89209095
Arthur <a.hovanesyan@gmail.com>
2017-12-16 22:19:20 +0100

    Normalized the data to show averages.

--- app/histo.js
+++ app/histo.js
@@ -1,3 +1,5 @@
+historgramData = []
+
 function grouped(arr) {
     var a = [], b = [], prev;
 
@@ -11,11 +13,13 @@ function grouped(arr) {
         }
         prev = arr[i];
     }
-
     return [a, b];
 }
 
-function countKeys()
+function update(date1, date2) {
+
+}
+
 
 function dateIsBetween(x, date1, date2) {
   months = {
@@ -51,48 +55,82 @@ function dateIsBetween(x, date1, date2) {
 //   return data;
 // }
 
-function calculateFrequencies(disturbances, key) {
-  Plotly.d3.csv("../data/processedWeather.csv", function(data2){
-    // tempfrequency = grouped(data.map(x => parseInt(x["min_temp"])));
-    processData(disturbances, []);
-    // processData(data, tempfrequency);
+
+
+function calculateFrequencies(disturbances) {
+  Plotly.d3.csv("../data/processedWeather.csv", function(weather){
+    mintempfreq = grouped(weather.map(x => parseInt(x["min_temp"])));
+    avgtempfreq = grouped(weather.map(x => parseInt(x["avg_temp"])));
+    maxwindgustfreq = grouped(weather.map(x => parseInt(x["max_windgust"])));
+    totalprecipitationfreq = grouped(weather.map(x => parseInt(x["total_precipitation"])));
+    humidityfreq = grouped(weather.map(x => parseInt(x["humidity"])));
+    windspeedreq = grouped(weather.map(x => parseInt(x["windspeed"])));
+    minsightreq = grouped(weather.map(x => parseInt(x["min_sight"])));
+
+    // processData(disturbances, []);
+    frequencies = {
+      "Min Temp": mintempfreq,
+      "Avg Temp": avgtempfreq,
+      "Max Windgust": maxwindgustfreq,
+      "Total Precipitation": totalprecipitationfreq,
+      "Humidity": humidityfreq,
+      "Windspeed": windspeedreq,
+      "Min Sight": minsightreq
+    }
+    processData(disturbances, frequencies);
   });
 }
 
-Plotly.d3.csv("../data/weatherPerDisturbance.csv", function(data){ calculateFrequencies(data) } );
+Plotly.d3.csv("../data/weatherPerDisturbance.csv", function(disturbances){
+  calculateFrequencies(disturbances);
+});
 
 function processData(data, freq) {
 
-  console.log(data);
+  // var weatherDataPerCondition = {};
+  // // Init keys
+  // Object.keys(weatherData[0]).forEach((key) => weatherDataPerCondition[key] = []);
+  // weatherData.forEach((item) => {
+  //   Object.keys(item).forEach([key].push(item[key]))
+  // })
 
   windspeed = grouped(data.map(x => parseInt(x["Windspeed"] )));
   minsight = grouped(data.map(x => parseInt(x["Min Sight"] )));
   windgust = grouped(data.map(x => parseInt(x["Max Windgust"] )));
   mintemp = grouped(data.map(x => parseInt(x["Min Temp"])));
+  avgtemp = grouped(data.map(x => parseInt(x["Avg Temp"])));
   humidity = grouped(data.map(x => parseInt(x["Humidity"] )));
+  totalprecipitation = grouped(data.map(x => parseInt(x["Total Precipitation"] )));
 
-  // Normalize
-  // mintemp.forEach(function(item) {
-  //
-  // });
-  // //
-  // var counted = 0;
-  // function count(item) {
-  //   if(item === -15) {
-  //     counted++;
-  //   }
-  // }
-  // mintemp.forEach(count);
-  // console.log(counted);
+  console.log(grouped(data.map(x => parseInt(x["Min Temp"]))));
+
+  windspeed.label = "Windspeed";
+  minsight.label = "Min Sight";
+  windgust.label = "Max Windgust";
+  mintemp.label = "Min Temp";
+  avgtemp.label = "Avg Temp";
+  humidity.label = "Humidity";
+  totalprecipitation.label = "Total Precipitation";
+
+  [windspeed, minsight, windgust, mintemp, avgtemp, humidity, totalprecipitation]
+    .forEach(function(column) {
+      for (var i = 0; i < column[1].length; i++) {
+        column[1][i] = column[1][i] / freq[column.label][1][i];
+      }
+  })
 
   x1 = { data: mintemp[0], label: "Min Temp" };
-  y1 = { data: mintemp[1], label: "Count" };
+  y1 = { data: mintemp[1], label: "Average amount of disturbances" };
   x2 = { data: minsight[0], label: "Min Sight" };
-  y2 = { data: minsight[1], label: "Count" };
+  y2 = { data: minsight[1], label: "Average amount of disturbances" };
   x3 = { data: windgust[0], label: "Max Windgust" };
-  y3 = { data: windgust[1], label: "Count" };
+  y3 = { data: windgust[1], label: "Average amount of disturbances" };
   x4 = { data: humidity[0], label: "Humidity" };
-  y4 = { data: humidity[1], label: "Count" };
+  y4 = { data: humidity[1], label: "Average amount of disturbances" };
+  x4 = { data: avgtemp[0], label: "Avg Temp" };
+  y4 = { data: avgtemp[1], label: "Average amount of disturbances" };
+  x2 = { data: totalprecipitation[0], label: "Total Precipitation" };
+  y2 = { data: totalprecipitation[1], label: "Average amount of disturbances" };
   // y = {
   //   data: minsight,
   //   label: "Min Sight"
